f4f1c14fff88eb52fa7a33e044882580b5044397
fireflg <96112649+fireflg@users.noreply.github.com>
2022-01-06 04:26:25 +0300

    Update solve.py

--- solve.py
+++ solve.py
@@ -1,8 +1,13 @@
-John wants to decorate the walls of a room with wallpaper. He wants a fool-proof method for getting it right.
-
-John knows that the rectangular room has a length of l meters, a width of w meters, a height of h meters. The standard width of the rolls he wants to buy is 52 centimeters. The length of a roll is 10 meters. He bears in mind however, that it’s best to have an extra length of wallpaper handy in case of mistakes or miscalculations so he wants to buy a length 15% greater than the one he needs.
-
-Last time he did these calculations he got a headache, so could you help John?
-
-Task
-Your function wallpaper(l, w, h) should return as a plain English word in lower case the number of rolls he must buy.
+def wallpaper(l, w, h):
+    # your code
+    num2words1 = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', \
+            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', \
+            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', \
+            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+    summary = ((l * h * 2 + w * h * 2) * 1.15 / 5.2)
+    summary = round(summary)
+    if 1 <= summary < 19:
+        return num2words1[summary]
+    elif 20 <= summary <= 99:
+        tens, below_ten = divmod(summary, 10)
+        return num2words2[tens - 2] + '-' + num2words1[below_ten]
